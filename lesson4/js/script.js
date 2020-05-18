0ec65494193660e8d990def750c04cd655072974
jdshaw1278 <sha10016@byui.edu>
2020-05-17 23:07:26 -0600

    lesson 4: upload 3

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,3 +1,16 @@
 function toggleMenu() {
     document.getElementById("dropdown").classList.toggle("hide")
 }
+
+function createDate() {
+    weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var d = new Date();
+    var weekday = d.getDay() 
+    var dayOfMonth = d.getDate();
+    var month = d.getMonth();
+    var year = d.getFullYear();
+
+    var fullDate = weekdays[weekday] + ', ' + dayOfMonth + " " + months[month] + " " + year;
+    document.getElementById("current_date").innerHTML = fullDate;
+}
