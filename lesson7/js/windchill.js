6e64130be32828cf1315329df5e2f750eced77bc
jdshaw1278 <sha10016@byui.edu>
2020-06-04 18:53:18 -0600

    first upload lesson 7 lazyload page

---
+++ lesson7/js/windchill.js
@@ -0,0 +1,15 @@
+var tempF = parseFloat(document.getElementById("fahrenheit").value);
+var speed = parseFloat(document.getElementById("wind").value);
+
+var chillF;
+var chillC;
+if (tempF > 50 || speed < 3.0) {
+    chillF = "NA";
+    chillC = "NA";
+} else {
+    chillF = 35.74 + (0.6215*tempF) - (35.75 * (speed ** 0.16)) + (0.4275 * tempF * (speed ** 0.16));        
+    chillC = (tempF - 32) * (5/9);
+}
+     
+document.getElementById("windchillF").innerHTML = chillF;
+document.getElementById("windchillC").innerHTML = chillC;
