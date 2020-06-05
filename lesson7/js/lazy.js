61d10233ad2367da4f9456ca74298c6f6b7c7071
jdshaw1278 <sha10016@byui.edu>
2020-06-05 12:23:21 -0600

    finally fixed blur!  Yay!

--- lesson7/js/lazy.js
+++ lesson7/js/lazy.js
@@ -1,4 +1,35 @@
-const images = document.querySelectorAll('[data-src]');
+const images = document.querySelectorAll("[data-src]");
+
+const loadImages = (image) => {
+    image.setAttribute("src",image.getAttribute("data-src"));
+    image.onload = () => {image.removeAttribute("data-src");}
+}
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 150px 0px"
+};
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                loadImages(entry.target);
+                imgObserver.unobserve(entry.target);
+            }
+        })
+    }, imgOptions);
+    images.forEach((img)=>{
+        imgObserver.observe(img)
+    })
+} else {
+    images.forEach(image => {
+        loadImages(image)
+    });
+}
+
+
+/*const images = document.querySelectorAll('[data-src]');
 
 function preloadImage(img) {
     const src = img.getAttribute('data-src');
@@ -14,7 +45,7 @@ const imgOptions = {
     rootMargin: "0px 0px 200px 0px"
 };
 
-const imgObserver = new IntersectionObserver ((entries,imgObserver) => {
+const imgObserver = new IntersectionObserver ((entries, imgObserver) => {
     entries.forEach(entry => {
         if (!entry.isIntersecting) {
             return;
@@ -27,5 +58,5 @@ const imgObserver = new IntersectionObserver ((entries,imgObserver) => {
 
 images.forEach(image => {
     imgObserver.observe(image);
-});
+});*/
 
